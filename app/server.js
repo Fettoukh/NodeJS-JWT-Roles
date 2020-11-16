8dcb7ff7e9dd3a5f40df860ef10f54e2c05d83d3
Fettoukh <medamine.fettoukh@gmail.com>
2020-11-16 21:38:56 +0100

    added role check

--- app/server.js
+++ app/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const db = require("./models");
 
 const app = express();
+const Role = db.role;
 
 var corsOptions = {
   origin: "http://localhost:8081",
@@ -16,10 +18,43 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+db.sequelize.sync();
+
+/*initial() function helps us to create 3 rows in database.
+In development, you may need to drop existing tables and re-sync database. So you can use force: true as code above.
+
+For production, just insert these rows manually and use sync() without parameters to avoid dropping data:*/
+
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and Resync Db");
+//   initial();
+// });
+
+// function initial() {
+//   Role.create({
+//     id: 1,
+//     name: "user",
+//   });
+
+//   Role.create({
+//     id: 2,
+//     name: "moderator",
+//   });
+
+//   Role.create({
+//     id: 3,
+//     name: "admin",
+//   });
+// }
+
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to fettoukh application." });
 });
+
+// routes
+require("./routes/auth.routes")(app);
+require("./routes/user.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
